b625a4d086c8b6f3299c25cc47dcdd4ea4853466
Trojan0x <j.dauda@alustudent.com>
2024-04-15 22:08:02 +0200

    Time: 124 ms (35.46%) | Memory: 17.4 MB (63.33%) - LeetSync

--- 838-design-linked-list/design-linked-list.py
+++ 838-design-linked-list/design-linked-list.py
@@ -1,32 +1,48 @@
+class Node:
+    def __init__(self, val=0, nextNode=None):
+        self.val = val
+        self.next = nextNode
 class MyLinkedList:
 
     def __init__(self):
-        self.store = []
+        self.head = Node(0) 
+        self.size = 0
         
 
     def get(self, index: int) -> int:
-        if index > len(self.store) -1:
+        if index >= self.size:
             return -1
-        return self.store[index]
+        curr = self.head
+        for _ in range(index + 1):
+            curr = curr.next
+        return curr.val
         
 
     def addAtHead(self, val: int) -> None:
-        self.store.insert(0, val)
+        self.addAtIndex(0, val)
 
     def addAtTail(self, val: int) -> None:
-        self.store.append(val)
+        self.addAtIndex(self.size, val)
 
     def addAtIndex(self, index: int, val: int) -> None:
-        if index < len(self.store):
-            self.store.insert(index, val)
-        elif index == len(self.store):
-            self.store.append(val)
-        
+        if index > self.size:
+            return
+        prev = self.head
+        for _ in range(index):
+            prev = prev.next
+        newNode = Node(val, prev.next)
+        prev.next = newNode
+        self.size += 1
+
 
     def deleteAtIndex(self, index: int) -> None:
-        if index > len(self.store) - 1:
+        if index >= (self.size):
             return
-        self.store.pop(index)
+        prev = self.head
+        for _ in range(index):
+            prev = prev.next
+        prev.next = prev.next.next
+        self.size -= 1
         
 
 
