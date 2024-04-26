ebe1d1eb7c3077c8512654c4cf3769742fdcf2b4
Trojan0x <j.dauda@alustudent.com>
2024-04-26 20:58:39 +0200

    Created 'LRU Cache' by A2SV Hub

---
+++ 26-Apr-2024/LRU Cache 134671.py
@@ -0,0 +1,22 @@
+# Problem: LRU Cache - https://leetcode.com/problems/lru-cache/
+
+from collections import OrderedDict
+
+class LRUCache:
+    __slots__ = ('data', 'capacity')
+
+    def __init__(self, capacity: int):
+        self.data: Dict[int, int] = OrderedDict()
+        self.capacity: int = capacity
+
+    def get(self, key: int) -> int:
+        return -1 if key not in self.data else self.data.setdefault(key, self.data.pop(key))
+
+    def put(self, key: int, value: int) -> None:
+        try:
+            self.data.move_to_end(key)
+            self.data[key] = value
+        except KeyError:
+            self.data[key] = value
+            if len(self.data) > self.capacity:
+                self.data.popitem(last=False)
