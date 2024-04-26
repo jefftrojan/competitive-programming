6e4522a1b9da3ab707a705d3355d7392cecb0557
Trojan0x <j.dauda@alustudent.com>
2024-04-26 22:23:36 +0200

    Created 'Daily Temperatures' by A2SV Hub

---
+++ 26-Apr-2024/Daily Temperatures 134773.py
@@ -0,0 +1,14 @@
+# Problem: Daily Temperatures - https://leetcode.com/problems/daily-temperatures/
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        st = []
+        res = [0] * len(temperatures)
+
+        for i in range(len(temperatures)):
+            while st and temperatures[i] > temperatures[st[-1]]:
+                idx = st.pop()
+                res[idx] = i - idx
+            st.append(i)
+        
+        return res
