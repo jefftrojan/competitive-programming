9d4a0bd9eb9053cfe4f7eca49c56480323a6bb47
Trojan0x <j.dauda@alustudent.com>
2024-04-27 20:52:26 +0200

    Created 'A - FIA Heist' by A2SV Hub

---
+++ A2SV Remote G5 Contest 12 27-Apr-2024/A - FIA Heist 135091.py
@@ -0,0 +1,16 @@
+# Problem: A - FIA Heist - https://codeforces.com/gym/520098/problem/A
+
+def decode_string(s):
+    decoded = []
+    for char in s:
+        if char == '<' and decoded:
+            decoded.pop() 
+        elif char != '<' and char != '-':
+            decoded.append(char) 
+    return ''.join(decoded)
+
+intercepted_string = input().strip()
+
+decoded_string = decode_string(intercepted_string)
+
+print(decoded_string)
