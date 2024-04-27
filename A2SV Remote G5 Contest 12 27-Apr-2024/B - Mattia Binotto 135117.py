d8920e37566bb7fb76e7e76adbe2293d440db27e
Trojan0x <j.dauda@alustudent.com>
2024-04-27 21:14:12 +0200

    Created 'B - Mattia Binotto' by A2SV Hub

---
+++ A2SV Remote G5 Contest 12 27-Apr-2024/B - Mattia Binotto 135117.py
@@ -0,0 +1,9 @@
+# Problem: B - Mattia Binotto - https://codeforces.com/gym/520098/problem/B
+
+input()
+
+a = b = 0
+c = sorted(list(map(int,input().split())))
+for i in c:
+    if a<=i: a+=i;b+=1
+print(b)
