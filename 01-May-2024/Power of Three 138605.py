1588766254cd656032d6ac116818b7a4fb73dd30
Trojan0x <j.dauda@alustudent.com>
2024-05-01 19:52:13 +0200

    Created 'Power of Three' by A2SV Hub

---
+++ 01-May-2024/Power of Three 138605.py
@@ -0,0 +1,9 @@
+# Problem: Power of Three - https://leetcode.com/problems/power-of-three/
+
+class Solution:
+    def isPowerOfThree(self, n):
+        if n<=0:
+            return False
+        while n%3==0:
+            n//=3
+        return n==1
