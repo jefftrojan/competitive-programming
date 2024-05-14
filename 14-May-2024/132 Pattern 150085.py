7c24cb849a43a3c9d9deb6b228a057cf0d0f7fbb
Trojan0x <j.dauda@alustudent.com>
2024-05-14 19:29:28 +0200

    Created '132 Pattern' by A2SV Hub

---
+++ 14-May-2024/132 Pattern 150085.py
@@ -0,0 +1,13 @@
+# Problem: 132 Pattern - https://leetcode.com/problems/132-pattern/
+
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack, third = [], float('-inf')
+        
+        for num in reversed(nums):
+            if num < third:
+                return True
+            while stack and stack[-1] < num:
+                third = stack.pop()
+            stack.append(num)
+        return False
