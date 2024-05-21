7f94372b9c50945c74889f06b15bb8d8c700772a
Trojan0x <j.dauda@alustudent.com>
2024-05-21 20:08:57 +0200

    Created 'Binary Search' by A2SV Hub

---
+++ 21-May-2024/Binary Search 156801.py
@@ -0,0 +1,16 @@
+# Problem: Binary Search - https://leetcode.com/problems/binary-search/description/
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+        while start <= end:
+            mid = start + (end - start) // 2
+            if target == nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+
+        return -1
