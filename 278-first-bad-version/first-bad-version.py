c41366a7204634cd7b32be36989472dd5492b173
Trojan0x <j.dauda@alustudent.com>
2024-05-21 22:04:25 +0200

    Time: 40 ms (16.90%) | Memory: 16.6 MB (12.53%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.py
@@ -0,0 +1,14 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        low=0
+        high=n
+        while(low<high):
+            mid=(low+high)//2
+            if isBadVersion(mid)==True:
+                high=mid
+            else:
+                low=mid+1
+        return low
