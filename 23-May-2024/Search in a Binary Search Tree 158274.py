f9e0a1450794e43e3bd4e908bf54241534af0432
Trojan0x <j.dauda@alustudent.com>
2024-05-23 22:43:12 +0200

    Created 'Search in a Binary Search Tree' by A2SV Hub

---
+++ 23-May-2024/Search in a Binary Search Tree 158274.py
@@ -0,0 +1,18 @@
+# Problem: Search in a Binary Search Tree - https://leetcode.com/problems/search-in-a-binary-search-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return
+        if root.val==val:
+            return root
+        if root.val<val:
+            return self.searchBST(root.right,val)
+        else:
+            return self.searchBST(root.left,val)
