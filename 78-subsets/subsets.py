52d6b6e1a9c6ae0a562277db13f29bf18222efae
Trojan0x <j.dauda@alustudent.com>
2024-06-07 20:10:40 +0200

    Time: 37 ms (55.87%) | Memory: 16.7 MB (74.78%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        if not nums:
+            return []
+        nums.sort()
+        res = [[]]
+        i = 0
+        while i < len(nums):
+            count = 1
+            while i + 1 < len(nums) and nums[i + 1] == nums[i]:
+                count += 1
+                i += 1
+            pre_length = len(res)
+            for j in range(pre_length):
+                sub_res = res[j][:]
+                for x in range(1, count + 1):
+                    if x > 0:
+                        sub_res.append(nums[i])
+                    res.append(sub_res[:])
+            i += 1
+        return res
