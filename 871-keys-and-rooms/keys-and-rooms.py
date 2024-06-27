b40c27448166feffa3c86176f6ba60fd8f89ed83
Trojan0x <j.dauda@alustudent.com>
2024-06-27 17:31:41 +0200

    Time: 64 ms (63.67%) | Memory: 17 MB (79.44%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        N = len(rooms)
+        visited = [False] * N
+
+        def visit(room):
+            visited[room] = True
+            for key in rooms[room]:
+                if not visited[key]:
+                    visit(key)
+        visit(0)
+        return all(visited
+        )
