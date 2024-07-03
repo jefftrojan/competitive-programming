19e47fd55dec4ee6837f354ec623e42aca6ccb74
Trojan0x <j.dauda@alustudent.com>
2024-07-03 23:43:21 +0200

    Time: 436 ms (64.24%) | Memory: 19.7 MB (44.23%) - LeetSync

---
+++ 542-01-matrix/01-matrix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m,n = len(mat),len(mat[0])
+        q = deque((i,j) for j in range(n) for i in range(m) if mat[i][j] == 0)
+        map = [[0 if mat[i][j] == 0 else None for j in range(n)] for i in range(m)]
+        while q:
+            i,j = q.popleft()
+            for n_i,n_j in [(i+1,j),(i-1,j),(i,j+1),(i,j-1)]:
+                if 0 <= n_i < m and 0 <= n_j < n and map[n_i][n_j] == None:
+                    map[n_i][n_j] = map[i][j] + 1
+                    q.append((n_i,n_j))
+        return map
