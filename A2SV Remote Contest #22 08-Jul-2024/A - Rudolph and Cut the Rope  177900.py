ad922032c41463438dbf0e2213ecc3e0abbbd865
Trojan0x <j.dauda@alustudent.com>
2024-07-08 21:32:29 +0200

    Created 'A - Rudolph and Cut the Rope ' by A2SV Hub

---
+++ A2SV Remote Contest #22 08-Jul-2024/A - Rudolph and Cut the Rope  177900.py
@@ -0,0 +1,32 @@
+# Problem: A - Rudolph and Cut the Rope  - https://codeforces.com/gym/533316/problem/A
+
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    
+    index = 0
+    t = int(data[index])
+    index += 1
+    
+    results = []
+    
+    for _ in range(t):
+        k = int(data[index])
+        index += 1
+        cnt = 0
+        
+        for _ in range(k):
+            a = int(data[index])
+            b = int(data[index + 1])
+            index += 2
+            if a - b > 0:
+                cnt += 1
+        
+        results.append(cnt)
+    
+    for result in results:
+        print(result)
+
+if __name__ == "__main__":
+    main()
