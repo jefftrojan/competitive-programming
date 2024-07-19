18a240a3639aeb66a3d226606032e68d39d4539a
Trojan0x <j.dauda@alustudent.com>
2024-07-19 21:49:38 +0200

    Created 'Two City Scheduling' by A2SV Hub

---
+++ 19-Jul-2024/Two City Scheduling 181987.py
@@ -0,0 +1,11 @@
+# Problem: Two City Scheduling - https://leetcode.com/problems/two-city-scheduling/
+
+class Solution(object):
+    def twoCitySchedCost(self, costs):
+        """
+        :type costs: List[List[int]]
+        :rtype: int
+        """
+        sm=sum([i for i,j in costs])
+        diff=sorted([j-i for i,j in costs])
+        return sm+sum(diff[:len(costs)//2])
