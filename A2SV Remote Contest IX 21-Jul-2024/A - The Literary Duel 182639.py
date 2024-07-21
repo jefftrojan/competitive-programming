d5875f367c0d21e763f54257359f6a841b491101
Trojan0x <j.dauda@alustudent.com>
2024-07-21 21:36:05 +0200

    Created 'A - The Literary Duel' by A2SV Hub

---
+++ A2SV Remote Contest IX 21-Jul-2024/A - The Literary Duel 182639.py
@@ -0,0 +1,32 @@
+# Problem: A - The Literary Duel - https://codeforces.com/gym/515369/problem/A
+
+def calculate_scores(n, books):
+    hermione_score = 0
+    harry_score = 0
+    left = 0
+    right = n - 1
+    hermione_turn = True
+
+    while left <= right:
+        if books[left] > books[right]:
+            chosen = books[left]
+            left += 1
+        else:
+            chosen = books[right]
+            right -= 1
+
+        if hermione_turn:
+            hermione_score += chosen
+        else:
+            harry_score += chosen
+
+        hermione_turn = not hermione_turn
+
+    return hermione_score, harry_score
+
+n = int(input())
+books = list(map(int, input().split()))
+
+hermione_final, harry_final = calculate_scores(n, books)
+
+print(f"{hermione_final} {harry_final}")
