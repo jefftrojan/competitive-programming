2aa70986cc765f1a61812b1caf6253f5fb84b111
Trojan0x <j.dauda@alustudent.com>
2024-07-21 21:35:48 +0200

    Created 'B -  The Piano Metamorphosis' by A2SV Hub

---
+++ A2SV Remote Contest IX 21-Jul-2024/B -  The Piano Metamorphosis 182638.py
@@ -0,0 +1,22 @@
+# Problem: B -  The Piano Metamorphosis - https://codeforces.com/gym/515369/problem/B
+
+def find_minimum_height_planks(n, k, heights):
+    if k > n:
+        return 0 
+
+    current_sum = sum(heights[:k])
+    min_sum = current_sum
+    min_index = 0
+
+    for i in range(1, n - k + 1):
+        current_sum = current_sum - heights[i-1] + heights[i+k-1]
+        if current_sum < min_sum:
+            min_sum = current_sum
+            min_index = i
+
+    return min_index + 1 
+
+n, k = map(int, input().split())
+heights = list(map(int, input().split()))
+result = find_minimum_height_planks(n, k, heights)
+print(result)
