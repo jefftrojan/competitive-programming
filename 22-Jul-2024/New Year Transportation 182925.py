99e7713b8c31c5046cf7e2f48562e1514b7cb846
Trojan0x <j.dauda@alustudent.com>
2024-07-22 21:06:59 +0200

    Created 'New Year Transportation' by A2SV Hub

---
+++ 22-Jul-2024/New Year Transportation 182925.py
@@ -0,0 +1,17 @@
+# Problem: New Year Transportation - https://codeforces.com/problemset/problem/500/A
+
+def main():
+    n, t = map(int, input().split())
+    c = list(map(int, input().split()))
+    p = 0
+
+    while p < t - 1:
+        p += c[p]
+
+    if p == t - 1:
+        print("YES")
+    else:
+        print("NO")
+
+if __name__ == "__main__":
+    main()
