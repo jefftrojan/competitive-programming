1e3a3db0b68db8a4e5f9fb4577c766e8286bf13c
Trojan0x <j.dauda@alustudent.com>
2024-08-06 16:11:06 +0200

    Created 'B - Operation 1' by A2SV Hub

---
+++ A2SV Remote G5 Contest #13 06-Aug-2024/B - Operation 1 187822.py
@@ -0,0 +1,19 @@
+# Problem: B - Operation 1 - https://codeforces.com/gym/521860/problem/B
+
+def min_operations(a, m):
+    min_ops = a // 2 + a % 2
+    
+    if min_ops % m == 0:
+        return min_ops
+    
+    next_multiple = ((min_ops // m) + 1) * m
+    
+    if next_multiple <= a:
+        return next_multiple
+    else:
+        return -1
+
+a, m = map(int, input().split())
+
+result = min_operations(a, m)
+print(result)
