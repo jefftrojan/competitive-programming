970acde27b907e782accfac7cedb050441e689da
Trojan0x <j.dauda@alustudent.com>
2024-08-06 16:12:58 +0200

    Created 'C - Operation 2' by A2SV Hub

---
+++ A2SV Remote G5 Contest #13 06-Aug-2024/C - Operation 2 187823.py
@@ -0,0 +1,21 @@
+# Problem: C - Operation 2 - https://codeforces.com/gym/521860/problem/C
+
+def min_operations(a, b):
+    operations = 0
+    
+    while b > a:
+        if b % 2 == 1:
+            b += 1
+            operations += 1
+        else:
+            b //= 2
+            operations += 1
+    
+    operations += a - b
+    
+    return operations
+
+a, b = map(int, input().split())
+
+result = min_operations(a, b)
+print(result)
