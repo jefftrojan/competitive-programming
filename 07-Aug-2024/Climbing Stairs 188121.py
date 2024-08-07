bbde8611fe6d3dfa14c6b23e2d4112740c14f542
Trojan0x <j.dauda@alustudent.com>
2024-08-07 11:07:09 +0200

    Created 'Climbing Stairs' by A2SV Hub

---
+++ 07-Aug-2024/Climbing Stairs 188121.py
@@ -0,0 +1,12 @@
+# Problem: Climbing Stairs - https://leetcode.com/problems/climbing-stairs/
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return 1
+        prev, curr = 1, 1
+        for i in range(2, n+1):
+            temp = curr
+            curr = prev + curr
+            prev = temp
+        return curr
