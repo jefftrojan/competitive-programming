9104fc6507034da395e02aa02be8d0a5e9ad651b
Trojan0x <j.dauda@alustudent.com>
2024-08-13 20:46:06 +0200

    Created 'Unique Paths' by A2SV Hub

---
+++ 13-Aug-2024/Unique Paths 191270.py
@@ -0,0 +1,14 @@
+# Problem: Unique Paths - https://leetcode.com/problems/unique-paths/
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+
+        aboveRow = [1] * n
+
+        for _ in range(m - 1):
+            currentRow = [1] * n
+            for i in range(1, n):
+                currentRow[i] = currentRow[i-1] + aboveRow[i]
+            aboveRow = currentRow
+        
+        return aboveRow[-1]
