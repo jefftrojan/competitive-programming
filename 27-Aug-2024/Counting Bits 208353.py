a47fd1674c339201ff9b42475bc6dadab938d177
Trojan0x <j.dauda@alustudent.com>
2024-08-27 20:30:33 +0200

    Created 'Counting Bits' by A2SV Hub

---
+++ 27-Aug-2024/Counting Bits 208353.py
@@ -0,0 +1,16 @@
+# Problem: Counting Bits - https://leetcode.com/problems/counting-bits/
+
+class Solution(object):
+
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        ans = [0] * (n + 1) 
+        for i in range(1, n + 1):
+            ans[i] = ans[i >> 1] + (i & 1)
+        return ans
+
+
+        
