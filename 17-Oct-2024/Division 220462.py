aa880f59f9cf37e09f51645e91b268407b0d4c07
Trojan0x <j.dauda@alustudent.com>
2024-10-17 23:51:26 +0200

    Created 'Division' by A2SV Hub

---
+++ 17-Oct-2024/Division 220462.py
@@ -0,0 +1,7 @@
+# Problem: Division - https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
